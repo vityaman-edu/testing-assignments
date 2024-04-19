718cf52271dbe2c26135663c0e010fb86e6b932c
vityaman <vityaman.dev@yandex.ru>
2024-04-19 09:22:56 +0300

    Added natural log series

---
+++ math/src/main/kotlin/ru/vityaman/math/real/mapping/logarithmic/NaturalLog.kt
@@ -0,0 +1,6 @@
+package ru.vityaman.math.real.mapping.logarithmic
+
+import ru.vityaman.math.real.mapping.Mapping
+
+interface NaturalLog : Mapping {
+}
---
+++ math/src/main/kotlin/ru/vityaman/math/real/mapping/logarithmic/series/NaturalLogSeries.kt
@@ -0,0 +1,26 @@
+package ru.vityaman.math.real.mapping.logarithmic.series
+
+import ru.vityaman.math.real.mapping.Mapping
+import kotlin.math.pow
+
+class NaturalLogSeries(private val limit: Int) : Mapping {
+    companion object {
+        private const val LIMIT_MAX: Int = 64
+        private val limitRange = (1 until LIMIT_MAX)
+    }
+
+    init {
+        require(limit in limitRange) {
+            "limit must be in range ${limitRange}, but $limit was given"
+        }
+    }
+
+    override operator fun invoke(x: Double): Double =
+        if (x.equals(-0.0)) {
+            Double.NEGATIVE_INFINITY
+        } else {
+            1 / (x / (x - 1) - (1 until limit).sumOf { k ->
+                2.0.pow(-k) * x.pow(2.0.pow(-k)) / (1 + x.pow(2.0.pow(-k)))
+            })
+        }
+}
